e57278838cbffaf4c608dbc83312d932c230fb42
eman <eman>
2014-05-11 06:45:53 +0000

    required packages

---
+++ required-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# packages required for cross-compilation
+
+sudo apt-get install libtool
+sudo apt-get install automake
+sudo apt-get install texinfo
+sudo apt-get install flex
+sudo apt-get install bison
+
+sudo apt-get install gcc-arm-linux-gnueabihf
+sudo apt-get install g++-arm-linux-gnueabihf
+
