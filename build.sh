cd558bd20abdf256d6d78abb480e5cda0d8af692
eman <eman>
2014-05-11 02:21:24 +0000

    arm

--- build.sh
+++ build.sh
@@ -27,6 +27,19 @@ then
   fi
 fi
 
+CROSS=arm-linux-gnueabihf
+CCPREFIX=${CROSS}-
+export ARCH=armhf
+
+export CXX=$CROSS-g++
+export CC=$CROSS-gcc
+export AR=$CROSS-ar
+export AS=$CROSS-as
+export RANLIB=$CROSS-ranlib
+export CC_host=gcc
+export CXX_HOST=g++
+
+
 cd `dirname $0`
 ENV_ROOT=`pwd`
 . ./env.source
@@ -46,7 +59,7 @@ cd $BUILD_DIR
 ../fetchurl "http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
-../fetchurl "http://webm.googlecode.com/files/libvpx-v1.1.0.tar.bz2"
+#../fetchurl "http://webm.googlecode.com/files/libvpx-v1.1.0.tar.bz2"
 ../fetchurl "http://downloads.sourceforge.net/project/faac/faac-src/faac-1.28/faac-1.28.tar.bz2"
 ../fetchurl "ftp://ftp.videolan.org/pub/x264/snapshots/last_x264.tar.bz2"
 ../fetchurl "http://downloads.xvid.org/downloads/xvidcore-1.3.2.tar.gz"
@@ -56,7 +69,7 @@ cd $BUILD_DIR
 
 echo "*** Building yasm ***"
 cd $BUILD_DIR/yasm*
-./configure --prefix=$TARGET_DIR
+./configure --host=arm-linux-gnueabihf --prefix=$TARGET_DIR
 make -j $jval
 make install
 
@@ -66,46 +79,46 @@ cd $BUILD_DIR/zlib*
 make -j $jval
 make install
 
-echo "*** Building bzip2 ***"
-cd $BUILD_DIR/bzip2*
-make
-make install PREFIX=$TARGET_DIR
+#echo "*** Building bzip2 ***"
+#cd $BUILD_DIR/bzip2*
+#make
+#make install PREFIX=$TARGET_DIR
 
 echo "*** Building libpng ***"
 cd $BUILD_DIR/libpng*
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=arm-linux-gnueabihf --enable-static --disable-shared
 make -j $jval
 make install
 
 # Ogg before vorbis
 echo "*** Building libogg ***"
 cd $BUILD_DIR/libogg*
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=arm-linux-gnueabihf --enable-static --disable-shared
 make -j $jval
 make install
 
 # Vorbis before theora
 echo "*** Building libvorbis ***"
 cd $BUILD_DIR/libvorbis*
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=arm-linux-gnueabihf --enable-static --disable-shared
 make -j $jval
 make install
 
 echo "*** Building libtheora ***"
 cd $BUILD_DIR/libtheora*
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=arm-linux-gnueabihf --enable-static --disable-shared
 make -j $jval
 make install
 
-echo "*** Building livpx ***"
-cd $BUILD_DIR/libvpx*
-./configure --prefix=$TARGET_DIR --disable-shared
-make -j $jval
-make install
+#echo "*** Building livpx ***"
+#cd $BUILD_DIR/libvpx*
+#./configure --prefix=$TARGET_DIR --disable-shared
+#make -j $jval
+#make install
 
 echo "*** Building faac ***"
 cd $BUILD_DIR/faac*
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=arm-linux-gnueabihf --enable-static --disable-shared
 # FIXME: gcc incompatibility, does not work with log()
 
 sed -i -e "s|^char \*strcasestr.*|//\0|" common/mp4v2/mpeg4ip.h
@@ -114,28 +127,28 @@ make install
 
 echo "*** Building x264 ***"
 cd $BUILD_DIR/x264*
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared --disable-opencl
+./configure --prefix=$TARGET_DIR --host=arm-linux-gnueabihf --enable-static --disable-shared --disable-opencl
 make -j $jval
 make install
 
 echo "*** Building xvidcore ***"
 cd "$BUILD_DIR/xvidcore/build/generic"
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=arm-linux-gnueabihf --enable-static --disable-shared
 make -j $jval
 make install
 #rm $TARGET_DIR/lib/libxvidcore.so.*
 
 echo "*** Building lame ***"
 cd $BUILD_DIR/lame*
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+./configure --prefix=$TARGET_DIR --host=arm-linux-gnueabihf --enable-static --disable-shared
 make -j $jval
 make install
 
-echo "*** Building opus ***"
-cd $BUILD_DIR/opus*
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared
-make -j $jval
-make install
+#echo "*** Building opus ***"
+#cd $BUILD_DIR/opus*
+#./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+#make -j $jval
+#make install
 
 # FIXME: only OS-specific
 rm -f "$TARGET_DIR/lib/*.dylib"
@@ -144,5 +157,38 @@ rm -f "$TARGET_DIR/lib/*.so"
 # FFMpeg
 echo "*** Building FFmpeg ***"
 cd $BUILD_DIR/ffmpeg*
-CFLAGS="-I$TARGET_DIR/include" LDFLAGS="-L$TARGET_DIR/lib -lm" ./configure --prefix=${OUTPUT_DIR:-$TARGET_DIR} --extra-cflags="-I$TARGET_DIR/include -static" --extra-ldflags="-L$TARGET_DIR/lib -lm -static" --extra-version=static --disable-debug --disable-shared --enable-static --extra-cflags=--static --disable-ffplay --disable-ffserver --disable-doc --enable-gpl --enable-pthreads --enable-postproc --enable-gray --enable-runtime-cpudetect --enable-libfaac --enable-libmp3lame --enable-libopus --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-bzlib --enable-zlib --enable-nonfree --enable-version3 --enable-libvpx --disable-devices
+CFLAGS="-I$TARGET_DIR/include" LDFLAGS="-L$TARGET_DIR/lib -lm" ./configure \
+--enable-cross-compile \
+--cross-prefix=${CCPREFIX} \
+--arch=${ARCH} \
+--target-os=linux \
+--prefix=${OUTPUT_DIR:-$TARGET_DIR} \
+--extra-cflags="-I$TARGET_DIR/include -static" \
+--extra-ldflags="-L$TARGET_DIR/lib -lm -static" \
+--extra-version=static \
+--disable-debug \
+--disable-shared \
+--enable-static \
+--extra-cflags=--static \
+--disable-ffplay \
+--disable-ffserver \
+--disable-doc \
+--enable-gpl \
+--enable-pthreads \
+--enable-postproc \
+--enable-gray \
+--enable-runtime-cpudetect \
+--enable-libfaac \
+--enable-libmp3lame \
+--disable-libopus \
+--enable-libtheora \
+--enable-libvorbis \
+--enable-libx264 \
+--enable-libxvid \
+--disable-bzlib \
+--enable-zlib \
+--enable-nonfree \
+--enable-version3 \
+--disable-libvpx \
+--disable-devices
 make -j $jval && make install
